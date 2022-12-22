67c1bf47cdedd6553e197c035e447993196d10db
Luis Carlos Rangel <luiscrangel19@gmail.com>
2022-12-21 19:42:25 -0500

    add relationship on initModels

--- src/models/initModels.js
+++ src/models/initModels.js
@@ -1,10 +1,28 @@
 const Users = require('./users.models')
 const RecoveryPasswords = require('./recoveryPasswords.models')
+const Conversations = require('./conversations.models')
+const Messages = require('./messages.models')
+const Participants = require('./participants.models')
 
 const initModels = () => {
     
     Users.hasMany(RecoveryPasswords)
     RecoveryPasswords.belongsTo(Users)
+       //? Un usuario tiene o envia muchos mensajes
+       Users.hasMany(Messages)
+       Messages.belongsTo(Users)
+       //? Un usuario tiene muchas conversaciones
+       Users.hasMany(Conversations)
+       Conversations.belongsTo(Users)
+       //? Un participantes solo tiene un usuario
+       Users.hasOne(Participants)
+       Participants.belongsTo(Users)
+       //? Una conversacion tiene muchos participantes
+       Conversations.hasMany(Participants)
+       Participants.belongsTo(Conversations)
+       //? Una conversacion tiene muchos mensajes
+       Conversations.hasMany(Messages)
+       Messages.belongsTo(Conversations)
 }
 
 module.exports = initModels
